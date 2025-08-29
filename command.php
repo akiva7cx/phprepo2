29bbb1f7ddbbf31bd0b3cc9b6be6e20ce3aa00a6
= <=>
2025-08-29 16:00:57 +0530

    The Edited One

---
+++ command.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<?php 
+$color="Red";
+$colo="White";
+$col="Black";
+echo"My car is ".$color. " in Color <br>";
+echo"My House is ".$colo. " in Color <br>";
+echo"My boat is ".$col. " in Color <br>";
+
+//This is the Single Line Comments
+
+#This is the Single Line Comments
+
+/*These are tools you’ll write PHP code in:
+
+VS Code (Free) – Lightweight, fast, with tons of PHP extensions like Intelephense and PHP Debug. */
+?>
+
+</body>
+</html>
